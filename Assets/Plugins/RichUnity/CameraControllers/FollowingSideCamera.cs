af5dec8e971f1694ecd5e126bbf2ef76ce063a40
Evgeny Osmet <111osmet111@gmail.com>
2017-02-26 03:09:24 +0500

    Following side camera was added

---
+++ Assets/Plugins/RichUnity/CameraControllers/FollowingSideCamera.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Assets.Plugins.RichUnity.CameraControllers {
+    [RequireComponent(typeof(Camera))]
+    public class FollowingSideCamera : MonoBehaviour {
+
+        public float DampTime;
+        public Transform Target;
+        public Vector3 Offset;
+
+        private new Camera camera;
+        private Vector3 velocity = Vector3.zero;
+
+        
+        void Awake() {
+            camera = GetComponent<Camera>();
+        }
+
+        // Update is called once per frame
+        void LateUpdate() {
+            if (Target) {
+                Vector3 targetViewportPosition = camera.WorldToViewportPoint(Target.position);
+                Vector3 delta = Target.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, targetViewportPosition.z));
+                Vector3 destination = transform.position + delta;
+                transform.position = Vector3.SmoothDamp(transform.position, destination + Offset, ref velocity, DampTime);
+            }
+        }
+    }
+}
