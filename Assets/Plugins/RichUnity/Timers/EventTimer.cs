bd21a7af7c2e10dd465078bdda1e411fcb7ee37e
Evgeny Osmet <111osmet111@gmail.com>
2017-03-02 12:48:58 +0500

    Timers

---
+++ Assets/Plugins/RichUnity/Timers/EventTimer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Assets.Plugins.RichUnity.Timers {
+    [Serializable]
+    public class EventTimer : LimitedTimer {
+        public event EventHandler Ended;
+
+        public EventTimer() {
+            
+        }
+
+        public override void End() {
+            if (Ended != null) {
+                Ended(this, EventArgs.Empty);
+            }
+            base.End();
+        }
+    }
+}
---
+++ Assets/Plugins/RichUnity/Timers/LimitedTimer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Assets.Plugins.RichUnity.Timers {
+    [Serializable]
+    public class LimitedTimer : Timer {
+        public float TimeLimit;
+
+        public bool Looped { get; set; }
+
+        public LimitedTimer() {
+            
+        }
+
+        public float CompletedPercent {
+            get {
+                return Time / TimeLimit;
+            }
+        }
+        
+        public float RemainingPercent {
+            get {
+                return RemainingTime / TimeLimit;
+            }
+        }
+
+        public float RemainingTime {
+            get {
+                return TimeLimit - Time;
+            }
+        }
+
+        public override void Update(float delta) {
+            base.Update(delta);
+            if (TimerOn && Time >= TimeLimit) {
+                End();
+            }
+        }
+
+        public override void End() {
+            if (Looped) {
+                Start();
+            } else {
+                base.End();
+            }
+        }
+
+        public String RemainingTimeMMSS {
+            get {
+                int totalSecs = (int) RemainingTime + 1;
+                //int hours = totalSecs / 3600;
+                int minutes = (totalSecs % 3600) / 60;
+                int seconds = totalSecs % 60;
+
+                return String.Format("%02d:%02d", minutes, seconds);
+            }
+        }
+    }
+}
