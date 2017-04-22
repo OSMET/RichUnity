10b4b3975d8078cf534f653e72c11f2132957a1c
Evgeny Osmet <111osmet111@gmail.com>
2017-04-22 17:41:46 +0500

    UnityEventTimer

---
+++ Assets/Plugins/RichUnity/Timers/UnityEventTimer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine.Events;
+
+namespace Assets.Plugins.RichUnity.Timers {
+    [Serializable]
+    public class UnityEventTimer : LimitedTimer {
+        public UnityEvent TimerEndedEvent;
+
+        public UnityEventTimer() {
+            
+        }
+
+        public override void End() {
+            TimerEndedEvent.Invoke();
+            base.End();
+        }
+    }
+}
