2dfb2de7c649ab62acf036d402a6cbfda98381dc
Evgeny Osmet <111osmet111@gmail.com>
2017-04-29 09:17:34 +0500

    AudioButton.cs

---
+++ Assets/Plugins/RichUnity/Audio/AudioButton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Plugins.RichUnity.Audio {
+    [RequireComponent(typeof(Button))]
+    public class AudioButton : MonoBehaviour {
+        public AudioClip OnClickAudioClip;
+
+        public void Start() {
+            GetComponent<Button>().onClick.AddListener(OnClick);
+        }
+
+        private void OnClick() {
+            AudioManager2D.Instance.PlaySound(OnClickAudioClip);
+        }
+    }
+}
