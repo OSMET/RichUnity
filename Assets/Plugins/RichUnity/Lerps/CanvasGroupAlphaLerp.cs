966e264ec00bc528971dfbef0e60435be631b97a
Evgeny Osmet <111osmet111@gmail.com>
2017-05-03 01:53:19 +0500

    CanvasGroupAlphaLerp

---
+++ Assets/Plugins/RichUnity/Lerps/CanvasGroupAlphaLerp.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Assets.Plugins.RichUnity.Lerps {
+    [RequireComponent(typeof(CanvasGroup))]
+    public class CanvasGroupAlphaLerp : Lerp<float> {
+        public override void ChangeValue(float percentage) {
+            GetComponent<CanvasGroup>().alpha = Mathf.Lerp(BeginValue, EndValue, percentage);
+        }
+    }
+}
