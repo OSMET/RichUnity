5be72d89e31f6367f342020f6f041f5c15f06b45
Evgeny Osmet <111osmet111@gmail.com>
2017-05-04 17:47:31 +0500

    AutoDisablePS, BurstPS

---
+++ Assets/Plugins/RichUnity/ParticleSystems/AutoDisablePS.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Assets.Plugins.RichUnity.ParticleSystems {
+    [RequireComponent(typeof(ParticleSystem))]
+    public class AutoDisablePS : MonoBehaviour {
+
+        public ParticleSystem ParticleSystem { get; set; }
+
+        public virtual void Awake() {
+            ParticleSystem = GetComponent<ParticleSystem>();
+        }
+
+        public void Update() {
+            if (!ParticleSystem.IsAlive()) {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
