1f69e73e4ab136d6e50cff6c7f3b07d0964cb25a
Evgeny Osmet <111osmet111@gmail.com>
2017-05-05 19:51:52 +0500

    TextFontSwitcher was added

---
+++ Assets/Plugins/RichUnity/UI/TextFontSwitcher.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.UI {
+    public class TextFontSwitcher : MonoBehaviour {
+
+        private Text text;
+        private int fontSize;
+        public Font NewFont;
+
+        public bool IsFontNew {get; private set; }
+
+        public void Awake() {
+            text = GetComponent<Text>();
+            fontSize = text.fontSize;
+        }
+
+        public void SetFont(bool isNew) {
+            if ((isNew && !IsFontNew) || (!isNew && IsFontNew)) {
+                SwitchFont();
+            }
+        }
+
+        public void SwitchFont() {
+            Font oldFont = text.font;
+            text.font = NewFont;
+            text.fontSize = fontSize;
+            IsFontNew = !IsFontNew;
+            NewFont = oldFont;
+        }
+    }
+}
