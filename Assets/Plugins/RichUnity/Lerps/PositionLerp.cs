968ad4018776f05ce74ca315d420b596ba807e84
Evgeny Osmet <111osmet111@gmail.com>
2017-05-18 07:48:08 +0500

    PositionLerp can use local position now RectPositionLerp

--- Assets/Plugins/RichUnity/Lerps/PositionLerp.cs
+++ Assets/Plugins/RichUnity/Lerps/PositionLerp.cs
@@ -2,8 +2,16 @@
 
 namespace Assets.Plugins.RichUnity.Lerps {
     public class PositionLerp : Lerp<Vector3> {
+
+        public bool LerpLocalPosition = false;
+
         public override void ChangeValue(float percentage) {
-            transform.position = Vector3.Lerp(BeginValue, EndValue, percentage);
+            var newPosition = Vector3.Lerp(BeginValue, EndValue, percentage);
+            if (LerpLocalPosition) {
+                transform.localPosition = newPosition;
+            } else {
+                transform.position = newPosition;
+            }
         }
     }
 }
