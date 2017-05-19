8a73f0856d2664f1d08025c705ab6b25a19d99f4
Evgeny Osmet <111osmet111@gmail.com>
2017-05-19 08:05:02 +0500

    ObjectSceneLimiter

---
+++ Assets/Plugins/RichUnity/Utils/ObjectSceneLimiter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Plugins.RichUnity.Utils {
+    public class ObjectSceneLimiter : MonoBehaviour {
+
+        public String[] SceneNames;
+
+        public void Awake() {
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+
+        public void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
+            if (!SceneNames.Contains(scene.name)) {
+                Destroy(gameObject);
+                Debug.Log("gerka");
+            }
+        }
+
+        public void OnDestroy() {
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+        }   
+
+    }
+}
