b0a8a2e5d002f61a6ce8beafbda9984f019ae7dc
Evgeny Osmet <111osmet111@gmail.com>
2017-05-27 16:30:34 +0500

    SoundSource volume

--- Assets/Plugins/RichUnity/Audio/SoundSource.cs
+++ Assets/Plugins/RichUnity/Audio/SoundSource.cs
@@ -13,13 +13,14 @@ namespace Assets.Plugins.RichUnity.Audio {
         }
 
         public void PlaySound() {
-            PlaySound(1f);
+            PlaySound(1f, 1f);
         }
         
-        public void PlaySound(float pitch) {
+        public void PlaySound(float pitch = 1f, float volume = 1f) {
             if (AudioManager.Instance.SoundOn) {
                 audioSource.mute = false;
                 audioSource.clip = SoundClip;
+                audioSource.volume = volume;
                 audioSource.pitch = pitch;
                 audioSource.Play();
             } else {
@@ -27,8 +28,12 @@ namespace Assets.Plugins.RichUnity.Audio {
             }
         }
 
-        public void PlaySoundRandomPitch(float lowPitchRange, float highPitchRange) {
-            PlaySound(Random.Range(lowPitchRange, highPitchRange));
+        public void PlaySoundRandomPitch() {
+            PlaySoundRandomPitch(0f, 1f, 1f);
+        }
+
+        public void PlaySoundRandomPitch(float lowPitchRange = 0f, float highPitchRange = 1f, float volume = 1f) {
+            PlaySound(Random.Range(lowPitchRange, highPitchRange), volume);
         }
     }
 }
