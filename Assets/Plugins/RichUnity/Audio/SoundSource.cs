92de9b943bcccdb08559b7b9b90f5da367ca0982
Evgeny Osmet <111osmet111@gmail.com>
2017-07-07 15:19:24 +0500

    SoundSource loop parameters and StopSound method

--- Assets/Plugins/RichUnity/Audio/SoundSource.cs
+++ Assets/Plugins/RichUnity/Audio/SoundSource.cs
@@ -13,16 +13,17 @@ namespace Assets.Plugins.RichUnity.Audio {
         }
 
         public void PlaySound() {
-            PlaySound(1f, 1f);
+            PlaySound(1f, 1f, false);
         }
         
-        public void PlaySound(float pitch = 1f, float volume = 1f) {
+        public void PlaySound(float pitch = 1f, float volume = 1f, bool loop = false) {
             if (gameObject.activeInHierarchy) {
                 if (AudioManager.Instance.SoundOn) {
                     audioSource.mute = false;
                     audioSource.clip = SoundClip;
                     audioSource.volume = volume;
                     audioSource.pitch = pitch;
+                    audioSource.loop = loop;
                     audioSource.Play();
                 } else {
                     audioSource.mute = true;
@@ -30,12 +31,16 @@ namespace Assets.Plugins.RichUnity.Audio {
             }
         }
 
-        public void PlaySoundRandomPitch() {
-            PlaySoundRandomPitch(0f, 1f, 1f);
+        public void StopSound() {
+            audioSource.Stop();
         }
 
-        public void PlaySoundRandomPitch(float lowPitchRange = 0f, float highPitchRange = 1f, float volume = 1f) {
-            PlaySound(Random.Range(lowPitchRange, highPitchRange), volume);
+        public void PlaySoundRandomPitch() {
+            PlaySoundRandomPitch(0f, 1f, 1f, false);
+        }
+
+        public void PlaySoundRandomPitch(float lowPitchRange = 0f, float highPitchRange = 1f, float volume = 1f, bool loop = false) {
+            PlaySound(Random.Range(lowPitchRange, highPitchRange), volume, loop);
         }
     }
 }
