189f83e515b7c8aa04bf000e1d97d952e508ccd7
Evgeny Osmet <111osmet111@gmail.com>
2017-07-23 07:30:59 +0500

    BinaryRangeSearcher

---
+++ Scripts/Utils/BinaryRangeSearcher.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace RichUnity.Utils {
+    public class BinaryRangeSearcher {
+        public int SearchLowerBoundIndex(List<float> values, float value) {
+            if (value < values[0]) {
+                return -1;
+            } else if (value >= values[values.Count - 1]) {
+                return values.Count - 1;
+            } else {
+                int left = 0;
+                int right = values.Count;
+                int mid;
+                while (true) {
+                    mid = left + (right - left) / 2;
+                    if (Mathf.Abs(value - values[mid]) < 0.000001f || mid == left) {
+                        if (mid == right || value < values[left]) {
+                            return mid - 1;
+                        } else {
+                            return mid;
+                        }
+                    } else {
+                        if (value < values[mid]) {
+                            right = mid;
+                        } else {
+                            left = mid + 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
