c1dac133727bf2b33d2c2d7851f74da2b0c2918d
Evgeny Osmet <111osmet111@gmail.com>
2017-12-23 01:13:47 +0500

    AudioManager music now takes all source parameters on scene changing

--- Scripts/Audio/AudioManager.cs
+++ Scripts/Audio/AudioManager.cs
@@ -19,7 +19,28 @@ namespace RichUnity.Audio {
                 Instance = this;
             } else if (Instance != this) {
                 if (Instance.MusicSource.clip != MusicSource.clip) {
-                    Instance.PlayMusic(MusicSource.clip);
+                    Instance.MusicSource.mute = MusicSource.mute;
+                    Instance.MusicSource.bypassEffects = MusicSource.bypassEffects;
+                    Instance.MusicSource.bypassListenerEffects = MusicSource.bypassListenerEffects;
+                    Instance.MusicSource.bypassReverbZones = MusicSource.bypassReverbZones;
+                    Instance.MusicSource.playOnAwake = MusicSource.playOnAwake;
+                    
+                    Instance.MusicSource.priority = MusicSource.priority;
+                    Instance.MusicSource.volume = MusicSource.volume;
+                    Instance.MusicSource.pitch = MusicSource.pitch;
+                    Instance.MusicSource.panStereo = MusicSource.panStereo;
+                    Instance.MusicSource.spatialBlend = MusicSource.spatialBlend;
+                    Instance.MusicSource.reverbZoneMix = MusicSource.reverbZoneMix;
+
+                    Instance.MusicSource.dopplerLevel = MusicSource.dopplerLevel;
+                    Instance.MusicSource.spread = MusicSource.spread;
+                    Instance.MusicSource.rolloffMode = MusicSource.rolloffMode;
+                    Instance.MusicSource.minDistance = MusicSource.minDistance;
+                    Instance.MusicSource.maxDistance = MusicSource.maxDistance;
+
+                    if (MusicSource.playOnAwake) {
+                        Instance.PlayMusic(MusicSource.clip);
+                    }
                 }
                 Destroy(gameObject);
             }
