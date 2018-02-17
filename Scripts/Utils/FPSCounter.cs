93f43312460ec15cf85065e36f5c96682264c163
Evgeny Osmet <111osmet111@gmail.com>
2018-02-17 22:00:15 +0500

    Added reworked FPSCounter

--- Scripts/Utils/FPSCounter.cs
+++ Scripts/Utils/FPSCounter.cs
@@ -1,59 +1,57 @@
 ﻿using UnityEngine;
 
 namespace RichUnity.Utils {
-    [RequireComponent(typeof(GUIText))]
     public class FPSCounter : MonoBehaviour {
-
-        public static FPSCounter Instance;
+        public static FPSCounter Instance { get; private set; }
 
         public float UpdateDelta = 0.5f;
 
         private float timeleft;
         private float accumulator;
         private int frames;
+        private string text;
+        private GUIStyle textStyle;
 
-        private new GUIText guiText;
-
-        public void Awake() {
+        private void Awake() {
             if (Instance == null) {
                 Instance = this;
-                guiText = GetComponent<GUIText>();
-                guiText.enabled = false;
+                DontDestroyOnLoad(gameObject);
             } else if (Instance != this) {
                 Destroy(gameObject);
             }
-            DontDestroyOnLoad(gameObject);
         }
 
-        public void Start() {
+        private void Start() {
             timeleft = UpdateDelta;
         }
 
-        public void OnEnable() {
-            if (guiText != null) {
-                guiText.enabled = true;
+        
+        private void OnGUI() {
+            if (textStyle != null) {
+                GUI.Label(new Rect(Screen.width - 100, 100, Screen.width, textStyle.fontSize), text, textStyle);
             }
         }
 
-        public void OnDisable() {
-            if (guiText != null) {
-                guiText.enabled = false;
-            }
-        }
-
-        public void Update() {
+        private void Update() {
             timeleft -= Time.deltaTime;
             accumulator += Time.timeScale / Time.deltaTime;
             frames++;
 
             if (timeleft <= 0f) {
-                guiText.text = "FPS: " + (accumulator / frames).ToString("f2");
+                textStyle = new GUIStyle {
+                    alignment = TextAnchor.UpperLeft,
+                    fontSize = Screen.height * 2 / 75,
+                    normal = {textColor = new Color(0.0f, 1.0f, 0.0f, 1.0f)}
+                };
+
+                float msec = Time.deltaTime * 1000.0f;
+                float fps = accumulator / frames;
+                text = string.Format("{0:0.00} FPS\n{1:0.00} ms", fps, msec);
+                
                 timeleft = UpdateDelta;
                 accumulator = 0.0f;
                 frames = 0;
             }
         }
-
-
     }
 }
