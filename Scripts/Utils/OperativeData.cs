f17cd5480320902397762a0b93a041386b9f06aa
Evgeny Osmet <111osmet111@gmail.com>
2018-02-17 22:16:58 +0500

    Reworked OperativeData Reworked SceneLimitedObject.cs

--- Scripts/Utils/OperativeData.cs
+++ Scripts/Utils/OperativeData.cs
@@ -1,18 +1,19 @@
-﻿namespace RichUnity.Utils {
-    public class OperativeData : ObjectSceneLimiter {
-        public static OperativeData Instance;
+﻿using UnityEngine;
+
+namespace RichUnity.Utils {
+    public class OperativeData : MonoBehaviour {
+        public static OperativeData Instance { get; private set; }
 
         public Bundle Data { get; private set; }
 
-        public override void Awake() {
+        private void Awake() {
             if (Instance == null) {
                 Instance = this;
                 Data = new Bundle();
-                base.Awake();
+                DontDestroyOnLoad(gameObject);
             } else if (Instance != this) {
                 Destroy(gameObject);
             }
-            DontDestroyOnLoad(gameObject);
         }
     }
 }
---
+++ Scripts/Utils/SceneLimitedObject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RichUnity.Utils {
+    public class SceneLimitedObject : MonoBehaviour {
+        [Tooltip("Leave the array empty to disable limits.")]
+        public String[] SceneNames;
+
+        protected virtual void Awake() {
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+
+        protected virtual void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
+            if (SceneNames.Length > 0 && !SceneNames.Contains(scene.name)) {
+                Destroy(gameObject);
+            }
+        }
+
+        protected virtual void OnDestroy() {
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
+    }
+}
