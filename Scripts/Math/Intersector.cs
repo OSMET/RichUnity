f3f7cccd60fde156d5402c95db08d35e5c5f8df6
Evgeny Osmet <111osmet111@gmail.com>
2018-02-18 01:13:07 +0500

    Modified Vector2Utils, Vector3Utils, Intersector: methods replaced by extension methods

--- Scripts/Math/Intersector.cs
+++ Scripts/Math/Intersector.cs
@@ -2,7 +2,7 @@
 
 namespace RichUnity.Math {
     public static class Intersector {
-        public static Rect Intersection(Rect rectangle1, Rect rectangle2) {
+        public static Rect Intersection(this Rect rectangle1, Rect rectangle2) {
             float xMin = Mathf.Max(rectangle1.xMin, rectangle2.xMin);
             float yMin = Mathf.Max(rectangle1.yMin, rectangle2.yMin);
             float xMax = Mathf.Min(rectangle1.xMax, rectangle2.xMax);
--- Scripts/Math/Vector2Utils.cs
+++ Scripts/Math/Vector2Utils.cs
@@ -6,7 +6,7 @@ namespace RichUnity.Math {
         /** Returns the angle in degrees of this vector relative to the x-axis. Angles are towards the positive y-axis (typically
 	     *  counter-clockwise) and between 0 and 360. 
          */
-        public static float AngleX(Vector2 vector) {
+        public static float AngleX(this Vector2 vector) {
             float angle = AngleXRad(vector) * Mathf.Rad2Deg;
             if (angle < 0) {
                 angle += 360;
@@ -17,19 +17,19 @@ namespace RichUnity.Math {
         /** Returns the angle in radians of this vector (point) relative to the x-axis. Angles are towards the positive y-axis.
 	      * (typically counter-clockwise) 
           */
-        public static float AngleXRad(Vector2 vector) {
+        public static float AngleXRad(this Vector2 vector) {
             return Mathf.Atan2(vector.y, vector.x);
         }
 
         /** Rotates the Vector2 by the given angle, counter-clockwise assuming the y-axis points up.
          * Parameter degrees - the angle in degrees */
-        public static Vector2 Rotate(Vector2 vector, float degrees) {
+        public static Vector2 Rotate(this Vector2 vector, float degrees) {
             return RotateRad(vector, degrees * Mathf.Deg2Rad);
         }
 
         /** Rotates the Vector2 by the given angle, counter-clockwise assuming the y-axis points up.
          * Parameter radians - the angle in radians */
-        public static Vector2 RotateRad(Vector2 vector, float radians){
+        public static Vector2 RotateRad(this Vector2 vector, float radians){
             float cos = Mathf.Cos(radians);
             float sin = Mathf.Sin(radians);
 
