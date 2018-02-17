7b771edd639e1790c9e115a1785cac27298e4897
Evgeny Osmet <111osmet111@gmail.com>
2018-02-18 01:05:13 +0500

    Added UserInterfaceUtils.cs Added NetworkUtils.cs

---
+++ Scripts/Utils/UserInterfaceUtils.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace RichUnity.Utils {
+    public static class UserInterfaceUtils {
+        public static bool PointerOverUserInterface {
+            get {
+                PointerEventData eventDataCurrentPosition =
+                    new PointerEventData(EventSystem.current) {
+                        position = new Vector2(Input.mousePosition.x, Input.mousePosition.y)
+                    };
+                List<RaycastResult> results = new List<RaycastResult>();
+                EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
+                return results.Count > 0;
+            }
+        }
+    }
+}
