cc10f4ceec1bc9a34a9d3f86475372cc784924b3
Evgeny Osmet <111osmet111@gmail.com>
2018-05-14 02:42:39 +0500

    Added AudioMixerVolumeController

---
+++ Scripts/Audio/AudioMixerVolumeController.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+namespace RichUnity.Audio
+{
+    [CreateAssetMenu(fileName = "AudioMixerVolumeController", menuName = "Audio Mixer Volume Controller")]
+    public class AudioMixerVolumeController : ScriptableObject
+    {
+        [SerializeField]
+        private AudioMixer audioMixer;
+
+        public AudioMixer AudioMixer
+        {
+            get
+            {
+                return audioMixer;
+            }
+        }
+
+        public string VolumeParameterName;
+
+        private const float LowerVolumeBound = -80.0f;
+        private const float UpperVolumeBound = 20.0f;
+
+        public float Volume
+        {
+            get
+            {
+                if (muted)
+                {
+                    return volumeBeforeMute;
+                }
+                else
+                {
+                    return VolumeNoMutedGuards;
+                }
+            }
+            set
+            {
+                float newVolumeT = Mathf.Clamp01(value);
+                if (muted)
+                {
+                    volumeBeforeMute = newVolumeT;
+                }
+                else
+                {
+                    VolumeNoMutedGuards = newVolumeT;
+                }
+            }
+        }
+
+        private float VolumeNoMutedGuards
+        {
+            get
+            {
+                float volume = 0.0f;
+                if (AudioMixer)
+                {
+                    AudioMixer.GetFloat(VolumeParameterName, out volume);
+                }
+
+                volume = Mathf.InverseLerp(LowerVolumeBound, UpperVolumeBound, volume);
+                return volume;
+            }
+
+            set
+            {
+                if (AudioMixer)
+                {
+                    AudioMixer.SetFloat(VolumeParameterName,
+                        Mathf.Lerp(LowerVolumeBound, UpperVolumeBound, value));
+                }
+            }
+        }
+
+        private bool muted;
+        private float volumeBeforeMute;
+
+        public bool Muted
+        {
+            get
+            {
+                return muted;
+            }
+            set
+            {
+                muted = value;
+                if (muted)
+                {
+                    volumeBeforeMute = VolumeNoMutedGuards;
+                    VolumeNoMutedGuards = 0.0f;
+                }
+                else
+                {
+                    VolumeNoMutedGuards = volumeBeforeMute;
+                }
+            }
+        }
+
+        private void OnEnable()
+        {
+            muted = false;
+        }
+    }
+}
