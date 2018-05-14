c5268c2f681ee2c25c9bad5dcc280dea15c9a09f
Evgeny Osmet <111osmet111@gmail.com>
2018-05-14 06:22:02 +0500

    Added AudioPlugSource with PlayOnAwake and Loop support

---
+++ Scripts/Audio/AudioPlugSource.cs
@@ -0,0 +1,59 @@
+﻿using RichUnity.Audio.AudioPlugs;
+using UnityEngine;
+
+namespace RichUnity.Audio
+{
+    public class AudioPlugSource : MonoBehaviour
+    {
+        public AudioSource AudioSource { get; private set; }
+
+        public AudioPlug AudioPlug;
+
+        public bool PlayOnAwake;
+        public bool Loop;
+
+        private bool playBegan;
+
+        protected virtual void Awake()
+        {
+            AudioSource = gameObject.AddComponent<AudioSource>();
+            if (PlayOnAwake)
+            {
+                Play();
+            }
+        }
+
+        public void Play()
+        {
+            AudioPlug.Play(AudioSource);
+            if (Loop)
+            {
+                playBegan = true;
+            }
+            else
+            {
+                playBegan = false;
+            }
+        }
+
+        protected virtual void Update()
+        {
+            if (playBegan)
+            {
+                if (Loop)
+                {
+                    if (!AudioSource.isPlaying)
+                    {
+                        playBegan = false;
+                        Play();
+                    }
+                }
+                else
+                {
+                    playBegan = false;
+                }
+            }
+
+        }
+    }
+}
