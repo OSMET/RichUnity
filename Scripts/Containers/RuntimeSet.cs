8237b95f22d2dd9e873118d18e46dc375401be2a
Evgeny Osmet <111osmet111@gmail.com>
2018-05-15 03:57:03 +0500

    Added RuntimeSet scriptable object

---
+++ Scripts/Containers/RuntimeSet.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RichUnity.Containers
+{
+    public abstract class RuntimeSet<T> : ScriptableObject
+    {
+        [SerializeField]
+        private List<T> items = new List<T>();
+
+        public void Add(T item)
+        {
+            if (!Contains(item))
+            {
+                items.Add(item);
+            }
+        }
+
+        public void Remove(T item)
+        {
+            if (Contains(item))
+            {
+                items.Remove(item);
+            }
+        }
+
+        public bool Contains(T item)
+        {
+            return items.Contains(item);
+        }
+
+        public T Find(Predicate<T> match)
+        {
+            return items.Find(match);
+        }
+    }
+}
