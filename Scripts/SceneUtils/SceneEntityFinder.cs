4ed26e8653671ee962d377983a604e27a46d8ccf
Evgeny Osmet <111osmet111@gmail.com>
2018-05-24 02:35:48 +0500

    Modified SceneEntityFinder: Added methods CompareBySearchType, FindAll

--- Scripts/SceneUtils/SceneEntityFinder.cs
+++ Scripts/SceneUtils/SceneEntityFinder.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using RichUnity.Containers;
 
@@ -6,7 +7,8 @@ namespace RichUnity.SceneUtils
 {
     public static class SceneEntityFinder
     {
-        public static TEntity Find<TEntity, TValue>(List<TEntity> sceneEntities, string sceneName) where TEntity : SceneEntity<TValue>
+        public static TEntity Find<TEntity, TValue>(List<TEntity> sceneEntities, string sceneName)
+            where TEntity : SceneEntity<TValue>
         {
             var foundSceneEntity = sceneEntities.Find(sceneEntity =>
                 sceneEntity.SceneNameSearchType == SceneEntitySearchType.Equals &&
@@ -16,19 +18,14 @@ namespace RichUnity.SceneUtils
             {
                 foundSceneEntity = sceneEntities.Find(sceneEntity =>
                 {
-                    if (sceneEntity.SceneNameSearchType == SceneEntitySearchType.StartsWith)
+                    switch (sceneEntity.SceneNameSearchType)
                     {
-                        return sceneName.StartsWith(sceneEntity.SearchString);
-                    }
-
-                    if (sceneEntity.SceneNameSearchType == SceneEntitySearchType.EndsWith)
-                    {
-                        return sceneName.EndsWith(sceneEntity.SearchString);
-                    }
-
-                    if (sceneEntity.SceneNameSearchType == SceneEntitySearchType.Regex)
-                    {
-                        return Regex.IsMatch(sceneName, sceneEntity.SearchString);
+                        case SceneEntitySearchType.StartsWith:
+                            return sceneName.StartsWith(sceneEntity.SearchString);
+                        case SceneEntitySearchType.EndsWith:
+                            return sceneName.EndsWith(sceneEntity.SearchString);
+                        case SceneEntitySearchType.Regex:
+                            return Regex.IsMatch(sceneName, sceneEntity.SearchString);
                     }
 
                     return false;
@@ -38,13 +35,74 @@ namespace RichUnity.SceneUtils
             return foundSceneEntity;
         }
 
-
-        public static TEntity Find<TEntity, TValue>(RuntimeSet<TEntity> sceneEntities, string sceneName) where TEntity : SceneEntity<TValue>
+        public static TEntity Find<TEntity, TValue>(RuntimeSet<TEntity> sceneEntities, string sceneName)
+            where TEntity : SceneEntity<TValue>
         {
             return Find<TEntity, TValue>(sceneEntities.ToList(), sceneName);
         }
+        
+        public static bool CompareBySearchType(string sceneName, string searchString, SceneEntitySearchType sceneEntitySearchType)
+        {
+            switch (sceneEntitySearchType)
+            {
+                case SceneEntitySearchType.Equals:
+                    return sceneName.Equals(searchString);
+                case SceneEntitySearchType.StartsWith:
+                    return sceneName.StartsWith(searchString);
+                case SceneEntitySearchType.EndsWith:
+                    return sceneName.EndsWith(searchString);
+                case SceneEntitySearchType.Regex:
+                    return Regex.IsMatch(sceneName, searchString);
+            }
+            return false;
+        }
+
+        public static List<TEntity> FindAll<TEntity, TValue>(List<TEntity> sceneEntities, string sceneName)
+            where TEntity : SceneEntity<TValue>
+        {
+            var foundSceneEntity = sceneEntities.FindAll(sceneEntity =>
+            {
+                switch (sceneEntity.SceneNameSearchType)
+                {
+                    case SceneEntitySearchType.Equals:
+                        return sceneName.Equals(sceneEntity.SearchString);
+                    case SceneEntitySearchType.StartsWith:
+                        return sceneName.StartsWith(sceneEntity.SearchString);
+                    case SceneEntitySearchType.EndsWith:
+                        return sceneName.EndsWith(sceneEntity.SearchString);
+                    case SceneEntitySearchType.Regex:
+                        return Regex.IsMatch(sceneName, sceneEntity.SearchString);
+                }
+                return false;
+            });
+            return foundSceneEntity;
+        }
+        
+        public static TEntity[] FindAll<TEntity, TValue>(TEntity[] sceneEntities, string sceneName)
+            where TEntity : SceneEntity<TValue>
+        {
+            var foundSceneEntity = Array.FindAll(sceneEntities, sceneEntity =>
+            {
+                switch (sceneEntity.SceneNameSearchType)
+                {
+                    case SceneEntitySearchType.Equals:
+                        return sceneName.Equals(sceneEntity.SearchString);
+                    case SceneEntitySearchType.StartsWith:
+                        return sceneName.StartsWith(sceneEntity.SearchString);
+                    case SceneEntitySearchType.EndsWith:
+                        return sceneName.EndsWith(sceneEntity.SearchString);
+                    case SceneEntitySearchType.Regex:
+                        return Regex.IsMatch(sceneName, sceneEntity.SearchString);
+                }
+                return false;
+            });
+            return foundSceneEntity;
+        }
+        
+        public static List<TEntity> FindAll<TEntity, TValue>(RuntimeSet<TEntity> sceneEntities, string sceneName)
+            where TEntity : SceneEntity<TValue>
+        {
+            return FindAll<TEntity, TValue>(sceneEntities.ToList(), sceneName);
+        }
     }
-    
-    
-    
 }
