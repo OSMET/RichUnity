4287901fb75616267846625f27be125a3f5075d9
Evgeny Osmet <111osmet111@gmail.com>
2018-09-15 22:34:47 +0500

    Modified BinaryRangeSearch: removed redundant elses

--- Scripts/Ranges/BinaryRangeSearch.cs
+++ Scripts/Ranges/BinaryRangeSearch.cs
@@ -11,40 +11,35 @@ namespace RichUnity.Ranges
             {
                 return -1;
             }
-            else if (value >= values[values.Count - 1])
+
+            if (value >= values[values.Count - 1])
             {
                 return values.Count - 1;
             }
-            else
+
+            int left = 0;
+            int right = values.Count;
+            int mid;
+            while (true)
             {
-                int left = 0;
-                int right = values.Count;
-                int mid;
-                while (true)
+                mid = left + (right - left) / 2;
+                if (Mathf.Abs(value - values[mid]) < 0.000001f || mid == left)
                 {
-                    mid = left + (right - left) / 2;
-                    if (Mathf.Abs(value - values[mid]) < 0.000001f || mid == left)
+                    if (mid == right || value < values[left])
                     {
-                        if (mid == right || value < values[left])
-                        {
-                            return mid - 1;
-                        }
-                        else
-                        {
-                            return mid;
-                        }
+                        return mid - 1;
                     }
-                    else
-                    {
-                        if (value < values[mid])
-                        {
-                            right = mid;
-                        }
-                        else
-                        {
-                            left = mid + 1;
-                        }
-                    }
+
+                    return mid;
+                }
+
+                if (value < values[mid])
+                {
+                    right = mid;
+                }
+                else
+                {
+                    left = mid + 1;
                 }
             }
         }
