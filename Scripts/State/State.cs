0eacbdfd006719eec0ce84220cfa2e6da4e5e119
Evgeny Osmet <111osmet111@gmail.com>
2018-09-25 23:00:30 +0500

    Added State Pattern implementation's base classes: State, StateContext

---
+++ Scripts/State/State.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace RichUnity.State
+{
+    public abstract class State<TState> : MonoBehaviour where TState : State<TState>
+    {
+        public StateContext<TState> Context { get; set; }
+        
+        public bool Is<T>() where T : TState
+        {
+            return GetType() == typeof(T);
+        }
+        
+        public abstract void OnEnter(TState prevState, object parameter);
+
+        public abstract void OnExit(TState nextState);
+    }
+}
---
+++ Scripts/State/StateContext.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace RichUnity.State
+{
+	public class StateContext<TState> : MonoBehaviour where TState : State<TState>
+	{
+		public List<TState> States;
+		public int BeginStateIndex;
+		public bool BeginStateApplyOnEnter = true;
+	
+		public TState CurrentState { get; private set; }
+
+		protected virtual void Awake ()
+		{
+			foreach (var state in States)
+			{
+				state.Context = this;
+			}
+		
+			CurrentState = States[BeginStateIndex]; //begin state
+
+			if (BeginStateApplyOnEnter)
+			{
+				CurrentState.OnEnter(null, null);
+			}
+		}
+	
+		public virtual void SetState<T>(object parameter = null) where T : TState
+		{
+			var newState = States.Find(state => state.Is<T>());
+			var oldState = CurrentState;
+			CurrentState.OnExit(newState);
+			CurrentState = newState;
+			CurrentState.OnEnter(oldState, parameter);
+		}
+	}
+}
