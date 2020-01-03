d98c7269c266415aec544d170f45a17d8b978e61
Evgeny Osmet <111osmet111@gmail.com>
2020-01-03 12:48:22 +0500

    Deleted 2 useless singletons

--- Scripts/SaveLoad/SaveLoadManagerSingleton.cs
+++
@@ -1,9 +0,0 @@
-﻿using RichUnity.Singletons;
-
-namespace RichUnity.SaveLoad
-{
-    public class SaveLoadManagerSingleton : PersistentSingleton<SaveLoadManagerSingleton>
-    {
-        public SaveLoadManager SaveLoadManager;
-    }
-}
